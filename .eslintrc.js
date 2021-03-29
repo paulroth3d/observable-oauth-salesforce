0d6086efe4fa52bdf1b4d719e8e38fda5ed1c5c2
Paul Roth <proth@Pauls-Mac-mini.local>
2021-03-29 00:30:00 -0500

    UPDATE: use logger to cleanup logs

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   rules: {
     'arrow-body-style': 'off',
     'comma-dangle': 'off',
-    'no-unused-vars': ['error', { args: 'none' }]
-  },
+    'no-unused-vars': ['error', { args: 'none' }],
+    'no-trailing-spaces': 'off'
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
-/* eslint-disable no-console */
-/*
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-*/
-
 require('dotenv').config();
 
+const logger = require('pino')();
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
@@ -37,8 +24,8 @@ app.use(mw.oauthCallback({
   callback: (req, res) => {
     // log
     const sfoauth = req.sfoauth || res.locals.sfoauth;
-    const payload = sfoauth.payload;
-    console.log(`received callback: ${JSON.stringify(payload)}`);
+    const payload = sfoauth.payload; // eslint-disable-line prefer-destructuring
+    logger.info('received callback: %j', payload);
     
     // set data in session
     req.session.sfoauth = payload;
@@ -61,18 +48,18 @@ app.use(mw.oauthInitiation({
     req.session.save();
 
     // log
-    console.log('See if we have payload in session');
+    logger.info('See if we have payload in session');
     if (process.env.SKIP_SESSION === 'true') {
-      console.log('skipping the validation');
-      req.session.sfoauth = {totallyFakeSession:true};
+      logger.info('skipping the validation');
+      req.session.sfoauth = { totallyFakeSession: true };
       req.session.secret = 'keyboard cat';
       req.session.save();
     } else if (!req.session || !req.session.sfoauth) {
       // we don't
-      console.log('No payload found in session - returning false to initiate dance');
+      logger.info('No payload found in session - returning false to initiate dance');
       return false;
     }
-    console.log('We did - return true to continue middleware chain');
+    logger.info('We did - return true to continue middleware chain');
     return true;
   }
 }));
@@ -85,13 +72,13 @@ app.get('/', (req, res, next) => {
 
 app.get('/success', (req, res, next) => {
   try {
-    console.log('req.session.sfoauth', req.session.sfoauth);
+    logger.info('req.session.sfoauth: %j', req.session.sfoauth);
     
     const jsonString = JSON.stringify(req.session.sfoauth);
     const observableDomain = process.env.OBSERVABLE_DOMAIN;
-    res.render('./pages/success', {sfoauthJSON:jsonString, observableDomain});
-  } catch (err){
-    res.send(JSON.stringify({err: 'error occurred'}));
+    res.render('./pages/success', { sfoauthJSON: jsonString, observableDomain });
+  } catch (err) {
+    res.send(JSON.stringify({ err: 'error occurred' }));
   }
 });
 
